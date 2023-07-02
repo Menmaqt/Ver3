cdd8406ce03e42b13295fd83774a0ad8882af0d6
Menmaqt <138382220+Menmaqt@users.noreply.github.com>
2023-07-03 04:50:37 +0800

    Add files via upload

---
+++ menu/auto-reboot.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+
+clear 
+if [ ! -e /usr/local/bin/reboot_otomatis ]; then
+echo '#!/bin/bash' > /usr/local/bin/reboot_otomatis 
+echo 'tanggal=$(date +"%m-%d-%Y")' >> /usr/local/bin/reboot_otomatis 
+echo 'waktu=$(date +"%T")' >> /usr/local/bin/reboot_otomatis 
+echo 'echo "Server successfully rebooted on the date of $tanggal hit $waktu." >> /root/log-reboot.txt' >> /usr/local/bin/reboot_otomatis 
+echo '/sbin/shutdown -r now' >> /usr/local/bin/reboot_otomatis 
+chmod +x /usr/local/bin/reboot_otomatis
+fi
+clear
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m                     • AUTO-REBOOT MENU •                        \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e ""
+echo -e " [\e[36m(01)\e[0m] Set Auto-Reboot Every 1 Hour "
+echo -e " [\e[36m(02)\e[0m] Set Auto-Reboot Every 6 Hour "
+echo -e " [\e[36m(03)\e[0m] Set Auto-Reboot Every 12 Hour "
+echo -e " [\e[36m(04)\e[0m] Set Auto-Reboot Every 1 day "
+echo -e " [\e[36m(05)\e[0m] Set Auto-Reboot Every 1 week "
+echo -e " [\e[36m(06)\e[0m] Set Auto-Reboot Every 1 Month "
+echo -e " [\e[36m(07)\e[0m] Off Auto-Reboot "
+echo -e " [\e[36m(08)\e[0m] View Reboot Log "
+echo -e " [\e[36m(09)\e[0m] Remove Reboot Log "
+echo -e ""
+echo -e " [\e[31m(0)\e[0m] \e[31mBACK TO MENU\033[0m"
+echo -e ""
+echo -e "Press x or [ Ctrl+C ] • To-Exit"
+echo -e ""
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+read -p " Select menu : " x
+if test $x -eq 1; then
+echo "10 * * * * root /usr/local/bin/reboot_otomatis" > /etc/cron.d/reboot_otomatis
+echo "Auto-Reboot has been set every an hour."
+elif test $x -eq 2; then
+echo "10 */6 * * * root /usr/local/bin/reboot_otomatis" > /etc/cron.d/reboot_otomatis
+echo "Auto-Reboot has been successfully set every 6 hours."
+elif test $x -eq 3; then
+echo "10 */12 * * * root /usr/local/bin/reboot_otomatis" > /etc/cron.d/reboot_otomatis
+echo "Auto-Reboot has been successfully set every 12 hours."
+elif test $x -eq 4; then
+echo "10 0 * * * root /usr/local/bin/reboot_otomatis" > /etc/cron.d/reboot_otomatis
+echo "Auto-Reboot has been successfully set once a day."
+elif test $x -eq 5; then
+echo "10 0 */7 * * root /usr/local/bin/reboot_otomatis" > /etc/cron.d/reboot_otomatis
+echo "Auto-Reboot has been successfully set once a week."
+elif test $x -eq 6; then
+echo "10 0 1 * * root /usr/local/bin/reboot_otomatis" > /etc/cron.d/reboot_otomatis
+echo "Auto-Reboot has been successfully set once a month."
+elif test $x -eq 7; then
+rm -f /etc/cron.d/reboot_otomatis
+echo "Auto-Reboot successfully TURNED OFF."
+elif test $x -eq 8; then
+if [ ! -e /root/log-reboot.txt ]; then
+	clear
+    echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e "\E[40;1;37m        • AUTO-REBOOT LOG •        \e[0m"
+    echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e ""
+    echo "No reboot activity found"
+    echo -e ""
+    echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo ""
+    read -n 1 -s -r -p "Press any key to back on menu"
+    auto-reboot
+	else
+	clear
+    echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e "\E[40;1;37m        • AUTO-REBOOT LOG •        \e[0m"
+    echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e ""    
+	echo 'LOG REBOOT'
+	cat /root/log-reboot.txt
+    echo -e ""
+    echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo ""
+    read -n 1 -s -r -p "Press any key to back on menu"
+    auto-reboot    
+fi
+elif test $x -eq 9; then
+clear
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[40;1;37m        • AUTO-REBOOT LOG •        \e[0m"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""  
+echo "" > /root/log-reboot.txt
+echo "Auto Reboot Log successfully deleted!"
+echo -e ""
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+read -n 1 -s -r -p "Press any key to back on menu"
+auto-reboot 
+elif test $x -eq 0; then
+clear
+menu-set
+else
+clear
+echo ""
+echo "Options Not Found In Menu"
+echo ""
+read -n 1 -s -r -p "Press any key to back on menu"
+auto-reboot 
+fi
---
+++ menu/menu-domain.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+
+clear 
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m                     • DOMAIN MENU •                        \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e ""
+echo -e " [\e[36m(01)\e[0m] Change Domain "
+echo -e " [\e[36m(02)\e[0m] Renew Certificate "
+echo -e ""
+echo -e " [\e[31m(0)\e[0m] \e[31mBACK TO MENU\033[0m"
+echo -e   ""
+echo -e   "Press x or [ Ctrl+C ] • To-Exit"
+echo -e   ""
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m                                             \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e ""
+read -p " Select menu : " opt
+echo -e ""
+case $opt in
+1) clear ; add-host ;;
+2) clear ; certv2ray ;;
+0) clear ; menu ;;
+x) exit ;;
+*) echo "anda salah tekan" ; sleep 1 ; menu-domain ;;
+esac
